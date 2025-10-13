0faaca85dc27b0c335dd3b7c08d66ee45afc27fc
Nikhil Patel <nikhil.patel@ecomexperts.io>
2025-10-14 02:35:34 +0530

    some changes

--- shopping-cart/js/ProductCard.js
+++ shopping-cart/js/ProductCard.js
@@ -1,22 +1,24 @@
-class ProductCard extends HTMLElement{
+class ProductCard extends HTMLElement {
   constructor() {
     super();
 
-    const template = document.getElementById('product__template').content.cloneNode(true);
+    const template = document.getElementById("product__template").content.cloneNode(true);
 
-    this.id = this.getAttribute('id');
-    this.image = this.getAttribute('image');
-    this.title = this.getAttribute('title');
-    this.price = this.getAttribute('price');
+    this.id = this.getAttribute("id");
+    this.image = this.getAttribute("image");
+    this.title = this.getAttribute("title");
+    this.price = this.getAttribute("price");
 
-    const imageEl = template.querySelector('.product__image');
-    const titleEl = template.querySelector('.product__title');
-    const buttonEl = template.querySelector('.product__add-to-cart');
+    const imageEl = template.querySelector(".product__image");
+    const titleEl = template.querySelector(".product__title");
+    const buttonEl = template.querySelector(".product__add-to-cart");
 
-    if(imageEl) imageEl.src = this.image;
-    if(titleEl) titleEl.textContent = this.title;
-
-    buttonEl.addEventListener('click', this.addToCart.bind(this));
+    if (imageEl) 
+      imageEl.src = this.image;
+    if (titleEl) 
+      titleEl.textContent = this.title;
+    
+    buttonEl.addEventListener("click", this.addToCart.bind(this));
     this.appendChild(template);
   }
 
@@ -26,9 +28,16 @@ class ProductCard extends HTMLElement{
       title: this.title,
       quantity: 1,
       price: this.price
+    };
+
+    const itemInCart = window.cart.items.find(i => i.id === item.id);
+    if (itemInCart) {
+      itemInCart.quantity++;
+    } else {
+      window.cart.items.push(item);
     }
 
-    console.log(item)
+    console.log(window.cart.items);
   }
 }
 
